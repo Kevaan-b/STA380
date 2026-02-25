dd81a988254629590c65fd6ee4277d1525b2c665
eshan <eshanhussain2004@gmail.com>
2026-02-25 07:15:01 -0500

    Fixing some bugs

--- Linear_Regression/generate_data.R
+++ Linear_Regression/generate_data.R
@@ -1,40 +1,40 @@
-# Generate synthetic data (from uniform distribution between 0 and 1) for every independent variable 
-# present in <x_vector>. It returns 
+# Generate synthetic data (from uniform distribution between 0 and 1)
+# for every independent variable
+# present in <x_vector>. It returns
 
 generate_data <- function(x_vector, b_vector, values, sd, output_dir){
-    # Store generated x values here
-    x_values = list()
+  # Store generated x values here # nolint
+  x_values <- list()
 
-    # Need a X matrix. First column is 1's
-    X = matrix(rep(1, values), ncol = 1)
+  # Need a X matrix. First column is 1's # nolint: indentation_linter.
+  X <- matrix(rep(1, values), ncol = 1) # nolint
 
-    # Need a noise column
-    e = rnorm(values, 0, sd)
+  # Need a noise column # nolint: indentation_linter.
+  e <- rnorm(values, 0, sd)
 
-    # iterate through all independent x variables
-    for (x in x_vector){
+   # iterate through all independent x variables # nolint: indentation_linter.
+  for (x in x_vector){
 
-        # generate independent x data points and add to list
-        generated_x = runif(values, 0, 1)
-        x_values = append(x_values, list(generated_x))
+    # generate independent x data points and add to list
+    generated_x <- runif(values, 0, 1)
+    x_values <- append(x_values, list(generated_x))
 
-        # Add the generated data as a column to the matrix 
-        X = cbind(X, generated_x)
-    }
+    # Add the generated data as a column to the matrix
+    X <- cbind(X, generated_x) # nolint
+  }
 
-    # generate dependent y data points and add to list
-    if (ncol(X) != length(b_vector) | nrow(X) != length(e)){
-        return (-1)
-    }
+  # generate dependent y data points and add to list
+  if (ncol(X) != length(b_vector) || nrow(X) != length(e)) {
+    return(-1)
+  }
 
-    # Generated y values
-    y_values = (X %*% b_vector) + e
+  # Generated y values
+  y_values <- (X %*% b_vector) + e
 
-    # Put values in a textfile
-    df <- data.frame(do.call(cbind, x_values), y = y_values)
-    write.table(df, file = output_dir, sep = ",", row.names = FALSE)
+  # Put values in a textfile
+  df <- data.frame(do.call(cbind, x_values), y = y_values)
+  write.table(df, file = output_dir, sep = ",", row.names = FALSE)
 
-    return(0)
 }
 
 
